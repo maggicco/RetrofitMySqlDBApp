e752326ebbd65eef3482c741a75edf0548d7e443
maggicco <maggicco83@hotmail.fr>
2022-03-15 19:44:26 +0100

    Added model class

---
+++ app/src/main/java/com/maggicco/marvelapp/model/Hero.java
@@ -0,0 +1,28 @@
+package com.maggicco.marvelapp.model;
+
+public class Hero {
+
+    private String name;
+    private String imageUrl;
+
+    public Hero(String name, String imageUrl) {
+        this.name = name;
+        this.imageUrl = imageUrl;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+}
--- app/src/main/java/com/maggicco/marvelapp/ui/MainActivity.java
+++ app/src/main/java/com/maggicco/marvelapp/ui/MainActivity.java
@@ -1,8 +1,10 @@
-package com.maggicco.marvelapp;
+package com.maggicco.marvelapp.ui;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import com.maggicco.marvelapp.R;
 
 public class MainActivity extends AppCompatActivity {
 
